5eacb3e24e4137c34282d30c45a59ed75b1f5dd5
ghpopovici <ghpopovici@gmail.com>
2016-03-24 14:10:16 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	index.html

--- GhPsendEmail.js
+++
@@ -1,9 +0,0 @@
-function sendMail() {
-    var link = "mailto:ghpopovici@gmail.com"
-    			 /*+ "?cc=myCCaddress@example.com"*/
-             + "?subject=" + escape("Contact form")
-             + "&body=" + escape(document.getElementById('comments').value)
-             ;
-
-    window.location.href = link;
-}
