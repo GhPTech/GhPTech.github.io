967019aea4ae513ac75ae844bc6ded685defd224
ghpopovici <ghpopovici@gmail.com>
2016-06-25 11:28:50 +0200

    removed unecessary script

--- javascripts/GhPsendEmail.js
+++
@@ -1,9 +0,0 @@
-function sendMail() {
-    
-    var mailContent = 
-    	   "mailto:"+ escape(emailAddress)+
-           "?subject=" + escape("Contact form")+
-           "&body=" + escape(document.getElementById('comments').value);
-           
-    window.location.href = mailContent;
-}
