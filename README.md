fd41ba29dd352ee750f3ff49d4067289a85bc148
ghpopovici <ghpopovici@gmail.com>
2016-07-11 19:30:42 +0200

    cleaned up all files

--- README.md
+++
@@ -1,6 +0,0 @@
-# GhP web site
-GhP's web site is published at:
-
-http://ghptech.github.io
-
-http://www.ghp.tech
--- javascripts/translateFr.js
+++
@@ -1,6 +0,0 @@
-function replaceFr() {
-    //document.open("text/html","replace");
-    //document.write("<h2>Learning about the HTML DOM is fun!</h2>");
-    //document.close();
-    document.getElementById("demo").innerHTML = "Hello World!";
-}
