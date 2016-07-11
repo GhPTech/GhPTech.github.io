5e18874bd101410aa911bf018882d3d0a324f885
ghpopovici <ghpopovici@gmail.com>
2016-07-11 17:20:08 +0200

    keep the romanian version, removed english and french versions

--- selectDataPDO.php
+++ selectDataPDO.php
@@ -8,7 +8,7 @@ $username = "GhPvisitor";
 $password = "GhPpassword";
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=GhPvisitor", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=GhPdatabase", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully"; 
